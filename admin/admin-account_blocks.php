bb11c9321646732acc74c6c2f9da4bc00c1562cf
ferchange <hamidreza_mz@yahoo.com>
2014-01-06 11:35:53 +0330

    backup53

--- admin/admin-account_blocks.php
+++ admin/admin-account_blocks.php
@@ -10,6 +10,12 @@ require_once $index_dir.'include/common.php';
 
 require $index_dir.'include/code/code_encoding8anticache_headers.php';
 
+if(!isset($site_salt)) if(isset($_COOKIE['reg8log_site_salt'])) $site_salt=$_COOKIE['reg8log_site_salt'];
+else {
+	require $index_dir.'include/code/code_fetch_site_vars.php';
+	setcookie('reg8log_site_salt', $site_salt, 0, '/', null, $https, true);
+}
+
 require $index_dir.'include/code/admin/code_require_admin.php';
 
 $sort_fields=array('username', 'last_ip', 'first_attempt', 'last_attempt', 'username_exists');
@@ -17,17 +23,42 @@ require $index_dir.'include/code/admin/code_pagination_params.php';
 
 require_once $index_dir.'include/code/code_db_object.php';
 
-if(isset($_POST['admin_action'])) {
+//-------------
+require $index_dir.'include/config/config_admin.php';
+
+require $index_dir.'include/code/admin/code_check_password_entry_needed4admin.php';
+
+if(isset($password_check_needed)) {
+	$try_type='password';
+	require $index_dir.'include/code/code_check_captcha_needed4user.php';
+
+	if(isset($captcha_needed)) {
+		require $index_dir.'include/code/sess/code_sess_start.php';
+		$captcha_verified=isset($_SESSION['captcha_verified']);
+	}
+}
+//-------------
+
+if(isset($_POST['admin_action']) or isset($_POST['captcha'])) {
 
 	require $index_dir.'include/code/code_prevent_xsrf.php';
 
-	foreach($_POST as $auto=>$action) {
-		if($action=='del') $del[]=$auto;
-		else if($action=='unblock') $unblock[]=substr($auto, 3);
+	require $index_dir.'include/code/admin/code_captcha8password_check.php';
+	
+	if(!isset($err_msgs)) {
+	
+		require $index_dir.'include/code/admin/code_update_password_check.php';
+
+		foreach($_POST as $auto=>$action) {
+			if($action=='del') $del[]=$auto;
+			else if($action=='unblock') $unblock[]=substr($auto, 3);
+		}
+	
+		if(isset($unblock)) require $index_dir.'include/code/admin/code_unblock_accounts.php';
+		if(isset($del)) require $index_dir.'include/code/admin/code_delete_account_block_log_records.php';
+
 	}
 	
-	if(isset($unblock)) require $index_dir.'include/code/admin/code_unblock_accounts.php';
-	if(isset($del)) require $index_dir.'include/code/admin/code_delete_account_block_log_records.php';
 }
 
 $query="select * from `account_block_log`";
