3b427dc4da4ff5147082bb2ee50ef74be7b6b84f
unknown <hm@hm-28590e26d063.(none)>
2015-03-15 12:32:58 +0330

    backup109

--- change_lang.php
+++ change_lang.php
@@ -2,11 +2,11 @@
 if(ini_get('register_globals')) exit("<center><h3>Error: Turn that damned register globals off!</h3></center>");
 define('CAN_INCLUDE', true);
 
+require 'include/common.php';
+
 if(!isset($_SESSION['reg8log']['reg8log_antixsrf_token4get'], $_GET['antixsrf_token']) or $_SESSION['reg8log']['reg8log_antixsrf_token4get']!==$_GET['antixsrf_token']) exit('<h3 align=center>XSRF prevention mechanism triggered!</h3>');
 
 if(isset($_GET['setup'])) $setup_page=true;
-
-require 'include/common.php';
 
 if($lang=='en') setcookie('reg8log_lang', 'fa', time()+60*60*24*365, '/', null, HTTPS, true);
 else setcookie('reg8log_lang', 'en', time()+60*60*24*365, '/', null, HTTPS, true);
