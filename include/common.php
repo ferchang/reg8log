c23553d91f6c5b5b5981081bacb3ad38632b258d
unknown <hm@hm-28590e26d063.(none)>
2015-03-16 12:11:07 +0330

    backup113

--- include/common.php
+++ include/common.php
@@ -2,9 +2,28 @@
 if(ini_get('register_globals')) exit("<center><h3>Error: Turn that damned register globals off!</h3></center>");
 if(!defined('CAN_INCLUDE')) exit("<center><h3>Error: Direct access denied!</h3></center>");
 
+$req_time=time();
+
+ob_start();
+
 define('ROOT', str_replace('/include', '', str_replace('\\', '/', __DIR__)).'/');
 
-require ROOT.'include/config/config_common.php';
+require ROOT.'include/config/config_common.php';//TBR
+
+$error_log_file=ROOT.'file_store/error_log.txt';
+
+//-----------------------------
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('log_errors', 1);
+ini_set('error_log', $error_log_file);
+
+require ROOT.'include/class/class_class_loader.php';
+
+//$config=new config;
+
+//-----------------------------
 
 if($debug_mode) {
 	error_reporting(E_ALL);
@@ -12,9 +31,20 @@ if($debug_mode) {
 }
 else ini_set('display_errors', '0');
 
-$req_time=time();
+//---------------------------------
 
-ob_start();
+if($log_errors) {
+	if(!$debug_mode) error_reporting($log_errors);
+	//if debug_mode is on, error reporting level shouldn't be changed (it is set to E_ALL)
+	ini_set('log_errors', 1);
+	ini_set('error_log', $error_log_file);
+	//ini_set('ignore_repeated_errors', 1);
+	//ini_set('ignore_repeated_source', 1);
+	if(file_exists($error_log_file)) {
+		if(!is_writable($error_log_file)) trigger_error('reg8log: Error log file not writable!', E_USER_WARNING);
+	}
+	else if(!is_writable(dirname($error_log_file))) trigger_error('reg8log: Error log directory not writable!', E_USER_WARNING);
+}
 
 //----------- language ------------>
 
@@ -36,8 +66,6 @@ else {
 if(!empty($_SERVER['HTTPS']) and $_SERVER['HTTPS']!=='off' || $_SERVER['SERVER_PORT']==443) define('HTTPS', true);
 else define('HTTPS', false);
 
-require ROOT.'include/class/class_class_loader.php';
-
 if(isset($_COOKIE['reg8log_client_sess_key'])) $client_sess_key=$_COOKIE['reg8log_client_sess_key'];
 else $client_sess_key='';
 
@@ -48,23 +76,7 @@ if(!$client_sess_key) {
 	setcookie('reg8log_client_sess_key', $client_sess_key, 0, '/', null, HTTPS, true);
 }
 
-//$config=new config;
-
 //----------------------------------
-
-if($log_errors) {
-	if(!$debug_mode) error_reporting($log_errors);
-	//if debug_mode is on, error reporting level shouldn't be changed (it is set to E_ALL)
-	ini_set('log_errors', 1);
-	$error_log_file=ROOT.'file_store/error_log.txt';
-	ini_set('error_log', $error_log_file);
-	//ini_set('ignore_repeated_errors', 1);
-	//ini_set('ignore_repeated_source', 1);
-	if(file_exists($error_log_file)) {
-		if(!is_writable($error_log_file)) trigger_error('reg8log: Error log file not writable!', E_USER_WARNING);
-	}
-	else if(!is_writable(dirname($error_log_file))) trigger_error('reg8log: Error log directory not writable!', E_USER_WARNING);
-}
 
 ignore_user_abort(true);
 
