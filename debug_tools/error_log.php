d50f3dec335d106b0f5bb1783524ce40c9d98d64
unknown <hm@hm-28590e26d063.(none)>
2015-04-21 09:26:43 +0330

    backup193

--- debug_tools/error_log.php
+++ debug_tools/error_log.php
@@ -11,13 +11,17 @@ if(!config::get('debug_mode')) {
 
 $error_log_file=ROOT.'file_store/error_log.txt';
 
+if(!is_writable($error_log_file)) $not_writable='Error log file not writable!';
+
 if(isset($_POST['clear'])) {
 	if(!config::get('debug_mode') and config::get('admin_error_log_access')!==2) exit('Clear access to error log is not enabled in config file!');
 	require ROOT.'include/code/code_prevent_xsrf.php';
-	file_put_contents($error_log_file, '');
+	if(isset($not_writable)) $not_writable='Cannot clear error log file! Error log file not writable.';
+	else file_put_contents($error_log_file, '');
 }
 
-$logs=file_get_contents($error_log_file);
+if(!is_readable($error_log_file)) $logs=false;
+else $logs=file_get_contents($error_log_file);
 
 ?>
 <html>
@@ -34,13 +38,23 @@ textarea {
 	margin-bottom: 10px;
 	border: thin solid #000;
 	background: #aaa;
-	color: #000;
+	<?php
+	if($logs===false) echo "color: red;\n";
+	else echo "color: #000;\n";
+	if($logs==='' or $logs===false) echo "text-align: center;\n"; 
+	?>
 	padding: 5px;
-	<?php if($logs==='') echo "text-align: center;\n"; ?>
+	
 }
 a {
 	background: #aaa;
 	padding: 3px
+}
+#error {
+	color: red;
+	background: #000;
+	padding: 3px;
+	display: inline;
 }
 </style>
 <script>
@@ -50,9 +64,13 @@ function reload() {
 </script>
 </head>
 <body>
+<?php
+if(isset($not_writable)) echo "<center><h4 align=center id=error>$not_writable</h4></center>";
+?>
 <textarea readonly>
 <?php
 if($logs==='') echo "\n\nError log file is empty.";
+else if($logs===false) echo "\n\nError log file not readable!";
 else echo $logs;
 ?>
 </textarea>
